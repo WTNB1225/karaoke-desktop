6db6e2db5654e0873e872b7c4da8637d2c47cc99
WTNB1225 <144193938+WTNB1225@users.noreply.github.com>
2024-11-13 14:55:09 +0900

    描画の準備

--- src/pages/Play.tsx
+++ src/pages/Play.tsx
@@ -1,92 +1,90 @@
-import {useState} from "react";
-const map = new Map();
-map.set(440.0, "A4");
-map.set(466.16, "A#4");
-map.set(493.88, "B4");
-map.set(523.25, "C5");
-map.set(554.37, "C#5");
+import {useState, useEffect} from "react";
+
+type Data = {
+    note: string;
+    frequency: number;
+    start_time: number;
+    end_time: number;
+}
+
+const noteRange = ["C3", "C#3", "D3", "D#3", "E3", "F3", "F#3", "G3", "G#3", "A3", "A#3", "B3", "C4", "C#4", "D4", "D#4", "E4", "F4", "F#4", "G4", "G#4", "A4", "A#4", "B4", "C5", "C#5", "D5", "D#5", "E5", "F5", ];
+// 判定枠の上側のY座標 
+const noteLeneTop = [100, 120, 140, 160, 180, 200, 220, 240, 260, 280, 300, 320, 340, 360, 380, 400, 420, 440, 460, 480, 500, 520, 540, 560, 580, 600, 620, 640, 660, 680];
+const noteData: Data[] = [];
+
+function drawBall(ctx: CanvasRenderingContext2D, x: number, y: number) {
+    let posX = x;
+    let posY = y;
+    const radius = 7;
+    const intervalId = setInterval(() => {
+        console.log("drawBall");
+        // 前回の円を消す
+        ctx.clearRect(posX - radius - 1, posY - radius - 1, radius * 2 + 2, radius * 2 + 2);
+
+        // 新しい円を描く
+        ctx.beginPath();
+        ctx.fillStyle = "Red";
+        ctx.arc(posX, posY, radius, 0, Math.PI * 2, true);
+        ctx.fill();
+        ctx.closePath();
+
+        posY += 1;
+    }, 20);
+    return () => clearInterval(intervalId); // コンポーネントのアンマウント時にインターバルをクリア
+}
+
+function drawNote(ctx: CanvasRenderingContext2D, noteData: Data[]) {
+    ctx.fillStyle = "Red";
+    let note = "A4";
+    let note_index = noteRange.indexOf(note);
+    ctx.fillRect(100, noteLeneTop[note_index] + 2, 50, 15);
+}
+
 export default function Play() {
-    const [micFrequency, setMicFrequency] = useState<number>(0);
-    const [mediaFrequency, setMediaFrequency] = useState<number>(0);
-    async function analyzeMediaStream() {
-        try {
-            const mediaContext = new AudioContext();
-            const response = await fetch("/public/BELOVED.mp3");
-            const arrayBuffer = await response.arrayBuffer();
-            const analyzer = mediaContext.createAnalyser();
-            analyzer.fftSize = 1024*8;
-            const audioBuffer = await mediaContext.decodeAudioData(arrayBuffer);
-            const source = mediaContext.createBufferSource();
-            source.buffer = audioBuffer;
-            source.connect(analyzer);
-            source.start();
-            const dataArray = new Uint8Array(analyzer.frequencyBinCount);
-            const sampleRate =mediaContext.sampleRate;
-            const audio = new Audio("/public/BELOVED.mp3");
-            audio.play();
-            function getFrequency() {
-                analyzer.getByteFrequencyData(dataArray);
-                let maxIndex = 0;
-                for (let i = 0; i < dataArray.length; i++) {
-                    if (dataArray[i] > dataArray[maxIndex]) {
-                        maxIndex = i;
-                    }
-                }
-                const peakFrequency = (maxIndex * sampleRate) / analyzer.fftSize;
-                console.log("peakMediaFrequency:", peakFrequency);
-                setMediaFrequency(peakFrequency);
-        }
-            setInterval(getFrequency, 10);
-            
-        } catch (error) {
-            console.error("MediaStreamエラー:", error);
-        }
+    const [ctx, setCtx] = useState<CanvasRenderingContext2D | null>(null);
+    const [ballCtx, setBallCtx] = useState<CanvasRenderingContext2D | null>(null);
+    const [noteCtx, setNoteCtx] = useState<CanvasRenderingContext2D | null>(null);
+
+    const canvasWidth = 800;
+    const canvasHeight = 600;
+
+    noteData.push({note: "A4",frequency: 440.0, start_time: 1.0, end_time: 2.0});
+    noteData.push({note: "A4",frequency: 440.0, start_time: 2.0, end_time: 4.0});
+
+    // domが読み込まれた後に実行
+    useEffect(() => {
+        const canvas = document.getElementById("canvas") as HTMLCanvasElement;
+        const ballCanvas = document.getElementById("canvas1") as HTMLCanvasElement;
+        const noteCanvas = document.getElementById("canvas2") as HTMLCanvasElement;
+
+        setCtx(canvas.getContext("2d"));
+        setBallCtx(ballCanvas.getContext("2d"));
+        setNoteCtx(noteCanvas.getContext("2d"));
+    }, []);
+
+    // ノーツのレーンを描画している
+    if (ctx !== null) {
+        ctx.fillStyle = "White";
+
+        noteLeneTop.forEach((top, index) => {
+            ctx.fillStyle = "White";
+            ctx.fillRect(100, top, canvasWidth, 1);
+        })
+        ctx.fillStyle = "White";
+        ctx.fillRect(100, noteLeneTop[noteLeneTop.length - 1] + 20, canvasWidth, 1);
     }
-    async function analyzeAudioFrequency() {
-        try {
-            // ユーザーにマイクアクセスを要求
-            const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
-            // オーディオコンテキストを作成
-            const micContext = new AudioContext();
-            const micAnalyser = micContext.createAnalyser();
-            
-            // マイク入力をオーディオノードに接続
-            const source = micContext.createMediaStreamSource(stream);
-            source.connect(micAnalyser);
-            // FFTサイズを設定
-            micAnalyser.fftSize = 1024*8;
-            const bufferLength = micAnalyser.frequencyBinCount;
-            const dataArray = new Uint8Array(bufferLength);
-            // サンプリングレート取得
-            const sampleRate = micContext.sampleRate;
-            // 周波数データを取得して描画
-            function getFrequency() {
-                micAnalyser.getByteFrequencyData(dataArray);
-                let maxIndex = 0;
-                for (let i = 0; i < bufferLength; i++) {
-                    // 最大振幅のインデックスを見つける
-                    if (dataArray[i] > dataArray[maxIndex]) {
-                        maxIndex = i;
-                    }
-                }
-                const peakFrequency = (maxIndex * sampleRate) / micAnalyser.fftSize;
-                // ピーク周波数の計算と表示
-                if(dataArray[maxIndex] > 125 && peakFrequency < 1000 && peakFrequency > 80) {
-                    console.log("peakFrequency:", peakFrequency);
-                    setMicFrequency(peakFrequency);
-                }
-            }
-            setInterval(getFrequency, 1);
-        } catch (error) {
-            console.error("マイクアクセスエラー:", error);
-        }
+
+    // ノーツの描画
+    if (noteCtx !== null) {
+        drawNote(noteCtx, noteData);
     }
-    return (
+
+
+    return ( 
         <div className="flex w-full h-screen bg-gray-950">
-            <h1 className="mx-auto mt-10 text-lg">Karaoke</h1>
-            <button onClick={analyzeAudioFrequency}>start</button>
-            <button onClick={analyzeMediaStream}>start</button>
-            <audio src="/public/hz.wav"></audio>
+            <canvas id="canvas" width={1000} height={1000} className="absolute top-0 left-0"></canvas>
+            <canvas id="canvas1" width={1000} height={1000} className="absolute top-0 left-0"></canvas>
+            <canvas id="canvas2" width={1000} height={1000} className="absolute top-0 left-0"></canvas>
         </div>
     );
 }
